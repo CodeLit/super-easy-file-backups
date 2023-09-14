a8c47eca13b68f6896543299d7e4da1e26d86429
Vladimir <nlit@pm.me>
2023-09-15 03:13:49 +0600

    Changed GitHub workflow, tests now is working.
    Signed-off-by: Vladimir <nlit@pm.me>

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import {startBackups} from "./src/backups.js";
-import logger from "./src/class/Logger.js";
+import { startBackups } from './src/backups.js';
+import logger from './src/class/Logger.js';
 
-import cron from 'node-cron'
-
+import cron from 'node-cron';
 
 async function run() {
-    logger.log(`----- Starting backups -----`)
-    await startBackups(null, true)
-    logger.log(`----- Backups completed -----`)
+  logger.log(`----- Starting backups -----`);
+  await startBackups(null, true);
+  logger.log(`----- Backups completed -----`);
 }
 
 setTimeout(async () => {
-    await run();
-}, 1000)
+  await run();
+}, 1000);
 
 cron.schedule('0 3 * * *', run);
-
