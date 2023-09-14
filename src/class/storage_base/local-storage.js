c80e56a755aae5e773192376a81963e106c1f14e
Vladimir <nlit@pm.me>
2023-09-15 03:32:47 +0600

    Reformatting
    Signed-off-by: Vladimir <nlit@pm.me>

--- src/class/storage_base/local-storage.js
+++ src/class/storage_base/local-storage.js
@@ -1,64 +1,63 @@
-import fs from "fs-extra";
+import fs from 'fs-extra';
 
 class LocalFileBase {
+  /**
+   * Checks if a file or folder exists.
+   * @param {string} path - The path to check.
+   * @returns {boolean} - Returns true if the file or folder exists, false otherwise.
+   */
+  exists(path) {
+    // Check if the file or folder exists using the fs.existsSync() method.
+    return fs.existsSync(path);
+  }
 
-    /**
-     * Checks if a file or folder exists.
-     * @param {string} path - The path to check.
-     * @returns {boolean} - Returns true if the file or folder exists, false otherwise.
-     */
-    exists(path) {
-        // Check if the file or folder exists using the fs.existsSync() method.
-        return fs.existsSync(path);
-    }
+  /**
+   * Creates a directory at the given path.
+   *
+   * @param {string} path - The path where the directory should be created.
+   * @returns {Promise<void>} - A promise that resolves when the directory is created successfully.
+   */
+  async makeDir(path) {
+    // Create the directory recursively
+    await fs.promises.mkdir(path, { recursive: true });
+  }
 
-    /**
-     * Creates a directory at the given path.
-     *
-     * @param {string} path - The path where the directory should be created.
-     * @returns {Promise<void>} - A promise that resolves when the directory is created successfully.
-     */
-    async makeDir(path) {
-        // Create the directory recursively
-        await fs.promises.mkdir(path, {recursive: true});
-    }
+  /**
+   * Reads the contents of a directory and returns a sorted array of file names.
+   * @param {string} path - The path to the directory.
+   * @returns {string[]} - The sorted array of file names.
+   */
+  readDir(path) {
+    // Read the contents of the directory synchronously
+    let files = fs.readdirSync(path);
 
-    /**
-     * Reads the contents of a directory and returns a sorted array of file names.
-     * @param {string} path - The path to the directory.
-     * @returns {string[]} - The sorted array of file names.
-     */
-    readDir(path) {
-        // Read the contents of the directory synchronously
-        let files = fs.readdirSync(path);
+    // Sort the file names in ascending order
+    files.sort();
 
-        // Sort the file names in ascending order
-        files.sort();
+    // Return the sorted array of file names
+    return files;
+  }
 
-        // Return the sorted array of file names
-        return files;
-    }
+  /**
+   * Removes a file.
+   * @param {string} path - The path of the file to be removed.
+   * @returns {boolean} - Returns true if the file is successfully removed.
+   */
+  rmFile(path) {
+    fs.unlinkSync(path);
+    return true;
+  }
 
-    /**
-     * Removes a file.
-     * @param {string} path - The path of the file to be removed.
-     * @returns {boolean} - Returns true if the file is successfully removed.
-     */
-    rmFile(path) {
-        fs.unlinkSync(path);
-        return true;
-    }
-
-    /**
-     * Copy a file from one location to another.
-     *
-     * @param {string} fromFile - The path of the file to be copied.
-     * @param {string} toFile - The path where the file should be copied to.
-     * @return {undefined} - This function does not return anything.
-     */
-    copy(fromFile, toFile) {
-        return fs.copySync(fromFile, toFile, {})
-    }
+  /**
+   * Copy a file from one location to another.
+   *
+   * @param {string} fromFile - The path of the file to be copied.
+   * @param {string} toFile - The path where the file should be copied to.
+   * @return {undefined} - This function does not return anything.
+   */
+  copy(fromFile, toFile) {
+    return fs.copySync(fromFile, toFile, {});
+  }
 }
 
-export default LocalFileBase
+export default LocalFileBase;
--- src/class/storage_base/mega-storage.js
+++ src/class/storage_base/mega-storage.js
@@ -1,131 +1,130 @@
-import fs from "fs-extra";
+import fs from 'fs-extra';
 
 class MegaFileBase {
-    storage
+  storage;
 
-    /**
-     * Finds a file or folder
-     * @param path
-     * @returns MutableFile
-     */
-    find(path) {
-        // Split path into array
-        path = path.split('/')
+  /**
+   * Finds a file or folder
+   * @param path
+   * @returns MutableFile
+   */
+  find(path) {
+    // Split path into array
+    path = path.split('/');
 
-        let result = false
+    let result = false;
 
-        function findObject(nextObj) {
-            if (!nextObj.directory || !nextObj.children) return;
+    function findObject(nextObj) {
+      if (!nextObj.directory || !nextObj.children) return;
 
-            let found = nextObj.children.find(obj => obj.name === path[0])
+      let found = nextObj.children.find((obj) => obj.name === path[0]);
 
-            if (found) {
-                path.shift()
-                if (path.length === 0){
-                    result = found
-                    return;
-                }
-                findObject(found)
-            }
+      if (found) {
+        path.shift();
+        if (path.length === 0) {
+          result = found;
+          return;
         }
-
-        findObject(this.storage.root)
-
-        return result
+        findObject(found);
+      }
     }
 
-    /**
-     * Is file or folder exists
-     * @param path
-     * @returns {boolean}
-     */
-    exists(path) {
-        let obj = this.find(path)
-        return obj !== false
+    findObject(this.storage.root);
+
+    return result;
+  }
+
+  /**
+   * Is file or folder exists
+   * @param path
+   * @returns {boolean}
+   */
+  exists(path) {
+    let obj = this.find(path);
+    return obj !== false;
+  }
+
+  async makeDir(path) {
+    path = path.split('/');
+
+    async function findObject(nextObj) {
+      if (!nextObj.directory || path.length === 0) return;
+
+      let children = nextObj.children || [];
+      let found = children.find((obj) => obj.name === path[0]);
+
+      if (found === undefined) {
+        found = await nextObj.mkdir(path[0]);
+      }
+
+      await path.shift();
+
+      await findObject(found);
     }
 
-    async makeDir(path) {
-        path = path.split('/')
+    await findObject(this.storage.root);
+    return true;
+  }
 
-        async function findObject(nextObj) {
-            if (!nextObj.directory || path.length === 0) return
+  /**
+   * Reads a directory
+   * @param path
+   */
+  readDir(path) {
+    let filesInDir = [];
 
-            let children = nextObj.children||[]
-            let found = children.find(obj => obj.name === path[0])
+    let dir = this.find(path);
 
-            if (found === undefined) {
-                found = await nextObj.mkdir(path[0])
-            }
+    if (!dir) return false;
 
-            await path.shift()
-
-            await findObject(found)
-        }
-
-        await findObject(this.storage.root)
-        return true;
+    // Returns files and folders in root directory
+    for (const file of dir.children || []) {
+      if (!file.directory) filesInDir.push(file.name);
     }
 
-    /**
-     * Reads a directory
-     * @param path
-     */
-    readDir(path) {
+    // Возвращает все файлы и папки, в том числе корзину, Inbox и прочие
+    // for (const file of Object.values(this.storage.files)) {
+    //     console.warn(file.name)
+    // }
 
-        let filesInDir = []
+    filesInDir.sort();
 
-        let dir = this.find(path)
+    return filesInDir;
+  }
 
-        if (!dir) return false;
+  /**
+   * Removes a file. Return true if successfully
+   * @param path
+   * @returns {boolean}
+   */
+  async rmFile(path) {
+    let file = this.find(path);
+    if (file) {
+      await file.delete(true);
+      return true;
+    } else return false;
+  }
 
-        // Returns files and folders in root directory
-        for (const file of dir.children||[]) {
-            if (!file.directory)
-                filesInDir.push(file.name)
-        }
+  /**
+   * Uploads file to server
+   * @param fromFile
+   * @param toFile
+   */
+  async copy(fromFile, toFile) {
+    let toFileArr = toFile.split('/');
+    let toFileName = toFileArr.pop();
+    let dir = this.find(toFileArr.join('/'));
 
-        // Возвращает все файлы и папки, в том числе корзину, Inbox и прочие
-        // for (const file of Object.values(this.storage.files)) {
-        //     console.warn(file.name)
-        // }
+    const uploadStream = dir.upload(
+      {
+        name: toFileName,
+        allowUploadBuffering: true,
+      },
+      fs.createReadStream(fromFile),
+    );
 
-        filesInDir.sort()
-
-        return filesInDir
-    }
-
-    /**
-     * Removes a file. Return true if successfully
-     * @param path
-     * @returns {boolean}
-     */
-    async rmFile(path) {
-        let file = this.find(path)
-        if (file) {
-            await file.delete(true)
-            return true;
-        }
-        else
-            return false;
-    }
-
-    /**
-     * Uploads file to server
-     * @param fromFile
-     * @param toFile
-     */
-    async copy(fromFile, toFile) {
-        let toFileArr = toFile.split('/')
-        let toFileName = toFileArr.pop()
-        let dir = this.find(toFileArr.join('/'))
-
-        const uploadStream = dir.upload({
-            name: toFileName,
-            allowUploadBuffering: true,
-        }, fs.createReadStream(fromFile))
-
-        await uploadStream.complete
-    }
+    await uploadStream.complete;
+  }
 }
 
-export default MegaFileBase
+export default MegaFileBase;
