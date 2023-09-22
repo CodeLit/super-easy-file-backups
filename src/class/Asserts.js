2ae444a040efecbc80d4c4b7b7297dde71ca67a9
Vladimir <nlit@pm.me>
2023-09-22 23:52:02 +0600

    Asserts extended
    Signed-off-by: Vladimir <nlit@pm.me>

--- src/class/Asserts.js
+++ src/class/Asserts.js
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
 import assert from 'node:assert';
+import path from 'path';
 
 /**
  * Checks if a file exists.
@@ -11,7 +12,9 @@ export function fileExists(filePath) {
   // Check if the file exists
   if (!fs.existsSync(filePath)) {
     throw new assert.AssertionError({
-      message: `File ${filePath} does not exist.`,
+      message: `File ${path.basename(filePath)} does not exist.`,
+      expected: path.basename(filePath),
+      actual: fs.readdirSync(path.dirname(filePath)).join(', '),
     });
   }
 }
@@ -31,6 +34,22 @@ export function filesLength(folderPath, length) {
       message: `Folder ${folderPath} does not contain the expected number of files.`,
       actual: files.length,
       expected: length,
+    });
+  }
+}
+
+/**
+ * Checks if a folder is not empty.
+ *
+ * @param {string} folderPath - The path to the folder.
+ * @return {void} Throws an exception if the folder is empty.
+ */
+export function folderIsNotEmpty(folderPath) {
+  // Get the list of files in the folder
+  const files = fs.readdirSync(folderPath);
+  if (files.length === 0) {
+    throw new assert.AssertionError({
+      message: `Folder ${folderPath} is empty.`,
     });
   }
 }
--- test.js
+++ test.js
@@ -1,9 +1,19 @@
-import { backupsPath, startBackups } from './src/backups.js';
+import { backupsPath, projectsPath, startBackups } from './src/backups.js';
 import path from 'path';
 import FolderBackup from './src/class/FolderBackup.js';
-import { fileExists, filesLength } from './src/class/Asserts.js';
+import {
+  fileExists,
+  filesLength,
+  folderIsNotEmpty,
+} from './src/class/Asserts.js';
 
 const synchronously = true;
+
+const someProjectPath = path.normalize(projectsPath + '/some-project2');
+const testProjectPath = path.normalize(projectsPath + '/test-project');
+
+folderIsNotEmpty(someProjectPath);
+folderIsNotEmpty(testProjectPath);
 
 // There was a backup long time ago...
 await startBackups('2020-06-21', synchronously);
@@ -16,31 +26,64 @@ await startBackups('2021-08-24', synchronously);
 // Current TODAY will rewrite the old backups
 await startBackups(null, synchronously);
 
-const someProjectPath = path.normalize(backupsPath + '/some-project2');
+console.log('\x1b[32m--- Backups are created! ---\x1b[0m');
 
-filesLength(path.normalize(someProjectPath + '/daily'), 1);
-filesLength(path.normalize(someProjectPath + '/weekly'), 1);
-filesLength(path.normalize(someProjectPath + '/monthly'), 1);
-filesLength(path.normalize(someProjectPath + '/annually'), 1);
+folderIsNotEmpty(someProjectPath);
+folderIsNotEmpty(testProjectPath);
+
+const someProjectBackupPath = path.normalize(backupsPath + '/some-project2');
+
+folderIsNotEmpty(someProjectBackupPath);
+
+filesLength(path.normalize(someProjectBackupPath + '/daily'), 1);
+filesLength(path.normalize(someProjectBackupPath + '/weekly'), 1);
+filesLength(path.normalize(someProjectBackupPath + '/monthly'), 1);
+filesLength(path.normalize(someProjectBackupPath + '/annually'), 1);
 
 const todayDate = FolderBackup.formatISODate(new Date());
 const todayArchive = todayDate + '.tgz';
 
-fileExists(path.normalize(someProjectPath + '/daily/bkp_' + todayArchive));
-fileExists(path.normalize(someProjectPath + '/weekly/bkp_' + todayArchive));
-fileExists(path.normalize(someProjectPath + '/monthly/bkp_' + todayArchive));
-fileExists(path.normalize(someProjectPath + '/annually/bkp_' + todayArchive));
+fileExists(
+  path.normalize(someProjectBackupPath + '/daily/bkp_' + todayArchive),
+);
+fileExists(
+  path.normalize(someProjectBackupPath + '/weekly/bkp_' + todayArchive),
+);
+fileExists(
+  path.normalize(someProjectBackupPath + '/monthly/bkp_' + todayArchive),
+);
+fileExists(
+  path.normalize(someProjectBackupPath + '/annually/bkp_' + todayArchive),
+);
 
-const testProjectPath = path.normalize(backupsPath + '/test-project');
+console.log(
+  '\x1b[32m--- Asserts for some-project2 are successfully completed! ---\x1b[0m',
+);
 
-filesLength(path.normalize(testProjectPath + '/daily'), 4);
-filesLength(path.normalize(testProjectPath + '/weekly'), 4);
-filesLength(path.normalize(testProjectPath + '/monthly'), 3);
-filesLength(path.normalize(testProjectPath + '/annually'), 2);
+const testProjectBackupPath = path.normalize(backupsPath + '/test-project');
 
-fileExists(path.normalize(testProjectPath + '/daily/bkp_' + todayArchive));
-fileExists(path.normalize(testProjectPath + '/weekly/bkp_' + todayArchive));
-fileExists(path.normalize(testProjectPath + '/monthly/bkp_' + todayArchive));
-fileExists(path.normalize(testProjectPath + '/annually/bkp_' + todayArchive));
+folderIsNotEmpty(testProjectBackupPath);
+
+filesLength(path.normalize(testProjectBackupPath + '/daily'), 4);
+filesLength(path.normalize(testProjectBackupPath + '/weekly'), 4);
+filesLength(path.normalize(testProjectBackupPath + '/monthly'), 3);
+filesLength(path.normalize(testProjectBackupPath + '/annually'), 2);
+
+fileExists(
+  path.normalize(testProjectBackupPath + '/daily/bkp_' + todayArchive),
+);
+fileExists(
+  path.normalize(testProjectBackupPath + '/weekly/bkp_' + todayArchive),
+);
+fileExists(
+  path.normalize(testProjectBackupPath + '/monthly/bkp_' + todayArchive),
+);
+fileExists(
+  path.normalize(testProjectBackupPath + '/annually/bkp_' + todayArchive),
+);
+
+console.log(
+  '\x1b[32m--- Asserts for test-project are successfully completed! ---\x1b[0m',
+);
 
 console.log('\x1b[32m--- All test is successfully completed! ---\x1b[0m');
